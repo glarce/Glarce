a2ca0df02db960f1bd2d04b21c7d1445f5d5a335
trickypr <trickypr@users.noreply.github.com>
2019-02-08 08:48:42 +1100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-scene embedded arjs='detectionMode: mono_and_matrix; matrixCodeType: 3x3;'>
+    <slot v-for="(marker, index) in markers">
+      <slot v-if="marker.style == 'marker'">
+      <a-marker :preset="marker.value">
+        <a-box position="0 0.5 0" material="color: black;"></a-box>
+      </a-marker>
+      </slot>
+      <slot v-else-if="marker.style == 'barcode'">
+        <a-marker type='barcode' :value='marker.value'>
+          <a-box position="0 0.5 0" material="color: green;"></a-box>
+        </a-marker>
+      </slot>
+    </slot>
+    <a-entity camera></a-entity>
+  </a-scene>
 </template>
 
 <script>
-import AFrame from './components/AFrame.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    AFrame
+  },
+data() {
+  return {
+    markers: [
+      {style: 'marker', value: 'hiro'},
+      {style: 'marker', value: 'kanji'},
+      {style: 'barcode', value: 5}
+    ]
   }
 }
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
