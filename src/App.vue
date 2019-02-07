21b1c9defff000c9688ad58979657b83f0b06723
trickypr <trickypr@users.noreply.github.com>
2019-02-07 18:31:33 +1100

    Stuff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+   <router-link to="app">app</router-link>
+    
+    <router-view></router-view>
   </div>
 </template>
 
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,3 +1,33 @@
 <template>
-    
+  <div class="app">
+    <a-scene>
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+      <a-sky color="#ECECEC"></a-sky>
+    </a-scene>
+  </div>
 </template>
+
+<script>  
+import 'aframe'
+// @ is an alias to /src
+
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+      api: {}
+  }),
+  methods: {
+    gotAPI: function(api) {
+        this.api = api
+        console.log(api)
+    }
+  },
+  mounted: function() {
+    this.axios.get('api.json').then(this.gotAPI)
+  }
+};
+</script>
