5ca9dd81fbe1d90e7a18b5fa635e2ba4a461b66f
trickypr <trickypr@users.noreply.github.com>
2019-02-19 16:25:57 +1100

    Add api examples

---
+++ Code/otherFile.js
@@ -0,0 +1,21 @@
+export default {
+  style: 'barcode',
+  value: 7,
+  media: [{
+      type: 'video',
+      src: 'path/to/video.webm',
+      width: 5.3333333,
+      height: 3
+    },
+    {
+      type: 'obj',
+      src: 'path/to/obj.obj'
+    },
+    {
+      type: 'aframe',
+      html: 'path/to/file.html',
+      js: 'path/to/js.js',
+      assets: 'path.to/assets.html'
+    }
+  ]
+}
